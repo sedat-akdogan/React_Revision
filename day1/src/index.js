78ea0f7b0dc6374144cc5a28d3793f5788ee9adb
Sedat2121 <93882574+Sedat2121@users.noreply.github.com>
2022-12-23 18:06:27 +0000

    render an element instead of rendering App.js

--- day1/src/index.js
+++ day1/src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
 import './index.css';           // this line links the CSS file
-import App from './App';  // this line calls the App.js file
+// import App from './App';  // this line calls the App.js file
+
+const element = <h1>Rendering is redirected</h1>
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App/>);            // App.js is rendered by this line of code
+// root.render(<App/>);            // App.js is rendered by this line of code
+root.render(element);
