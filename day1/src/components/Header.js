ac52dd62fd8a82219b7267f776e096e8fd102fce
Sedat2121 <93882574+Sedat2121@users.noreply.github.com>
2022-12-23 19:48:26 +0000

    Header is created as a React Component

---
+++ day1/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class Header extends React.Component {
+    render() {
+        return(
+            <Header>
+                <h1>Header</h1>
+            </Header>
+        );
+    }
+}
