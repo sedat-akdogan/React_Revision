41c905693c5fafa26c26679d07ee69164516aae0
Sedat2121 <93882574+Sedat2121@users.noreply.github.com>
2022-12-23 22:35:06 +0000

    inline styling applied too

--- day1/src/components/Content.jsx
+++ day1/src/components/Content.jsx
@@ -3,8 +3,9 @@
 const Content = () => {
     return(
         <div>
-            <h2>React JS</h2>
-            <p>React is a JavaScript Library.</p>
+            {/* Inline Styling */}
+            <h2 style={{color: "red"}}>React JS</h2>
+            <p style={{backgroundColor: "green"}}>React is a JavaScript Library.</p>
             <img src="https://cdn.pixabay.com/photo/2017/02/15/13/40/tulips-2068692__340.jpg" alt="first image" />
         </div>
     );
