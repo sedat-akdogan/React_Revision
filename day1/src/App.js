516e77016f017b2fa7fb49d1ac108c9a73629336
Sedat2121 <93882574+Sedat2121@users.noreply.github.com>
2022-12-23 01:57:37 +0000

    the react project is ready

--- day1/src/App.js
+++ day1/src/App.js
@@ -1,7 +1,9 @@
 function App() {
   return (
+      // we have JSX format files only
     <div className="App">
       <h1>Hello REACT</h1>
+      <h2>The only working code body</h2>
     </div>
   );
 }
--- day1/src/index.js
+++ day1/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './index.css';           // this line links the CSS file
+import App from './App';  // this line calls the App.js file
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(<App/>);            // App.js is rendered by this line of code
