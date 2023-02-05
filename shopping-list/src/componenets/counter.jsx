b91dd5290f1ab5970cae22a560461d24482e36f0
Sedat2121 <93882574+Sedat2121@users.noreply.github.com>
2023-02-05 22:41:32 +0000

    list elements

--- shopping-list/src/componenets/counter.jsx
+++ shopping-list/src/componenets/counter.jsx
@@ -3,19 +3,25 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = {
-        count: 0
+        count: 0,
+        tags: ["tag1", "tag2", "tag3"]
     };
 
     render() {
-        let classes = " m-2 badge-";
-        classes += this.state.count === 0 ? "warning" : "primary";
         return (
         <div>
-            <h3 className={ classes }>{ this.formatCount() }</h3>
+            <span className={ this.getBadgeClasses() }>{ this.formatCount() }</span>
             <button className='btn btn-secondary btn-sm mt-5'>Increment</button>
+            <ul>{ this.state.tags.map(tag => <li key={tag}>{tag}</li>) }</ul>
         </div>
         );
     }
+    getBadgeClasses() {
+        let classes = " m-2 badge-";
+        classes += this.state.count === 0 ? "warning" : "primary";
+        return classes;
+    }
+
     formatCount() {
         const { count } = this.state;
         return count === 0 ? 'Zero' : count;
