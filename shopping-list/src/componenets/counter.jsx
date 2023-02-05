49ef4dda8a45068f97870070b5abc53d1ac3559c
Sedat2121 <93882574+Sedat2121@users.noreply.github.com>
2023-02-05 21:53:43 +0000

    formatCount function added

--- shopping-list/src/componenets/counter.jsx
+++ shopping-list/src/componenets/counter.jsx
@@ -2,13 +2,23 @@ import React, { Component } from 'react';
 
 
 class Counter extends Component {
+    state = {
+        count: 0
+    };
+
     render() {
+        let classes = " m-2 badge-";
+        classes += this.state.count === 0 ? "warning" : "primary";
         return (
         <div>
-            <h1>Hello World</h1>
-            <button>Increment</button>
+            <h3 className={ classes }>{ this.formatCount() }</h3>
+            <button className='btn btn-secondary btn-sm mt-5'>Increment</button>
         </div>
         );
+    }
+    formatCount() {
+        const { count } = this.state;
+        return count === 0 ? 'Zero' : count;
     }
 }
 
