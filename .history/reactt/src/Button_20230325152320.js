5da2798b7baee8c4037a9171d23cd1c71ac7a03f
Sedat2121 <93882574+Sedat2121@users.noreply.github.com>
2023-03-25 15:30:00 +0000

    Button component added

---
+++ .history/reactt/src/Button_20230325152320.js
@@ -0,0 +1,6 @@
+import React, { Component } from 'react';
+import './Button.css';
+
+class Button extends Component {
+    render
+}
---
+++ .history/reactt/src/Button_20230325152333.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import './Button.css';
+
+class Button extends Component {
+    render() {
+        return
+    }
+}
