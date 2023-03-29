7db33e5ec47cad6afc62e9e081b4a3838ea7818f
Sedat2121 <93882574+Sedat2121@users.noreply.github.com>
2023-03-29 03:47:47 +0100

    content inline styling

--- .history/sunday/src/components/Content_20230329033506.jsx
+++ .history/sunday/src/components/Content_20230329033506.jsx
@@ -3,10 +3,10 @@
 
 const Content = () => {
     return (
-        <div style={{ backgroundColor: "#92a8d1" }}>
-            <h2 style={{ color: "blue" }} >React JS</h2>
-            <p style={{ backgroundColor: "lightgreen" }}>React is JS Library</p>
-            <img style={{ textAlign: "center" }}
+        <div>
+            <h2 style={{ color: "blue", text-align:}} >React JS</h2>
+            <p>React is JS Library</p>
+            <img
                 src="https://images.unsplash.com/photo-1587620962725-abab7fe55159?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZ3JhbW1pbmd8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
                 alt="img1"
             />
--- .history/sunday/src/components/Content_20230329034522.jsx
+++ .history/sunday/src/components/Content_20230329034522.jsx
@@ -6,7 +6,7 @@ const Content = () => {
         <div style={{ backgroundColor: "#92a8d1" }}>
             <h2 style={{ color: "blue" }} >React JS</h2>
             <p style={{ backgroundColor: "lightgreen" }}>React is JS Library</p>
-            <img style={{ textAlign: "center" }}
+            <img style={{ textAlign }}
                 src="https://images.unsplash.com/photo-1587620962725-abab7fe55159?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZ3JhbW1pbmd8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
                 alt="img1"
             />
